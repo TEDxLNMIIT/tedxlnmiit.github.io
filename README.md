50f178f4b9d3b72e9abdfec2627b17e470bdeddb
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-11-17 14:52:30 +0530

    Add Teaser Website
    Adds teaser website for TEDx LNMIIT

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# tedxlnmiit.github.io
+# TEDx LNMIIT Website
+
+Official Website for TEDx LNMIIT.
+
+## Built With
+
+* [ParticlesJS](https://github.com/VincentGarreau/particles.js/)
+* [BootStrap](https://github.com/twbs/bootstrap)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ js/script.js
@@ -0,0 +1,112 @@
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#000000"
+    },
+    "shape": {
+      "type": "edge",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 3
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 0,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 0,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 160.3412060865523,
+      "color": "#5f0000",
+      "opacity": 1,
+      "width": 2.5
+    },
+    "move": {
+      "enable": true,
+      "speed": 4.4,
+      "direction": "bottom",
+      "random": false,
+      "straight": false,
+      "out_mode": "bounce",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 400.8530152163807,
+        "rotateY": 400.8530152163807
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "remove"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 121.81158184520177,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
